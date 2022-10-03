45a6be0b0237ae46fd8378f5c5042e1a46c111de
JungangHighschool <lhs109802@g.skku.edu>
2022-10-03 18:52:04 +0900

    [Subject] testcases [Description] making testcases [Issue] no txt files committed

---
+++ swe2015/test.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int main(void){
+	int pat_leng = 0, title = 0, scale = 1000000;
+	FILE *fp = NULL;
+	for(; title<1000; ++title){
+		scale += 10000;
+		fp = fopen(("testcases/"+to_string(title)+".txt").c_str(),"w+");
+		fprintf(fp,"%d\n",scale);
+		pat_leng = rand()%9 + 1;
+		for(int i = 0; i<pat_leng; ++i){
+			fprintf(fp,"%c", rand()%26+97);
+		}
+		fprintf(fp,"\n");
+		for(int i = 0; i< scale; ++i) fprintf(fp, "%c", rand()%26+97);
+		fclose(fp);
+	}
+	return 0;
+}
