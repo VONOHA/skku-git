3ce5bf5906e457a2b5204fceca597ea904d6fa05
JungangHighschool <lhs109802@g.skku.edu>
2022-10-06 01:59:44 +0900

    [Subject] problem1 [Description] done with stacks [Issue] None

--- dasf003/pa1/problem1/arrayModify.cc
+++ dasf003/pa1/problem1/arrayModify.cc
@@ -2,64 +2,62 @@
 
 //implement these functions
 void printArray(int *arr, int num){
-  //write your code here!
-	for(int i = 0; i<num; ++i){
-		cout<<arr[i]<<" ";
-	}
+	//write your code here!
+	for(int i = 0; i< num ; ++i) std::cout<<arr[i]<<" ";
 }
 void arrayModify(int *arr, int num){
-  //write your code here!
-	void push(int *arr, int *top, int num){
-		arr[++(*top)] = num;
-	}
-
-	int pop(int *arr, int *top){
-		if(top<0) return -1;
-		else{
-			int value = arr[(*top)--];
-			return value;
-		}
-	}
-
-
-	int stack_0[10]. stack_1[10], top_0 = -1, top_1 = -1, stack[10], top = -1, value, n = arr[0];
-
-	if(n%2) stack_0[++top_0] = n;
-	else stack_[++top_1] = n;
-
-	for(int i = 1; i<num; ++i){
-		
+	//write your code here!
+	/*std::cout<<"In ";
+	printArray(arr,num);
+	std::cout<<"\n";*/
+	int stack_0[10]={0}, stack_1[10]={0}, stack[10]={0}, top_0 = -1, top_1 = -1, top = -1, index = 0, n, value;
+	
+	for(int i = 0; i<num; ++i){
+		//printArray(stack_0,10);std::cout<<"\n";
+		//printArray(stack_1,10);std::cout<<"\n";
+		//printArray(stack,10);std::cout<<"\n";
 		n = arr[i];
-		
-		if(n%2){
-		
-		while((value = pop(stack_0,&top_0))<n && value != -1) push(stack,&top,value);
-		
-		push(stack_0,&top_0,n);
-		
-		while((value = pop(stack,&top)!=-1) push(stack_0,&top_0,value);
-		
-				}else {
-		
-		while((value = pop(stack_1,&top_1))>n && value != -1) push(stack,&top,value);
-		
-		push(stack_1,&top_1,n);
-		
-		while((value = pop(stack,&top)!=-1) push(stack_1,&top_1,value);
-				}
+		//std::cout<<n<<std::endl;
+		if( n%2 == 0 ){
+			//std::cout<<n<<std::endl;	
+			while (1){
+				if(top_0>-1){
+					value = stack_0[top_0--];
+					if(value > n){
+						stack[++top] = value;
+					}else{ 
+						stack_0[++top_0]=value;
+						break;
+					}
+				}else break;
+				
+			}
+
+			stack_0[++top_0]=n;
+			//printArray(stack_0,10); std::cout<<std::endl;	
+			while( top > -1 ) stack_0[++top_0] = stack[top--];
+			
+		}else{
+			//std::cout<<n<<std::endl;
+			while(1){
+				if(top_1>-1){
+					value = stack_1[top_1--];
+					if(value<n){
+						stack[++top] = value;
+					}else{
+						stack_1[++top_1] = value;
+						break;
+					}
+				}else break;
+				
+			}
+			stack_1[++top_1]= n;
+			//printArray(stack_1,10);std::cout<<std::endl;
+			while( top > -1) stack_1[++top_1] = stack[top--];
+		}
 	}
 
-	for(int i = 0; i<=top_1; ++i){
-	
-		arr[i] = stack_1[i];
-	
-		}
-	
-	for(int i = top_1 +1; i <= top_1+top_2+1; ++i){
-		
-		arr[i] = stack_0[i-top_1-1];
-	
-		}
-
+	while(top_1>-1) arr[index++] = stack_1[top_1--];
+	while(top_0>-1) arr[index++] = stack_0[top_0--];
 }
 
--- dasf003/pa1/problem1/main.cc
+++ dasf003/pa1/problem1/main.cc
@@ -1,16 +1,39 @@
-#include <iostream>
 #include "arrayModify.h"
-
-using namespace std;
 
 int main(){
   //write your code here!
   //you should call arrayModify function to modify array
   //you should call printArray function to print array
-  
-	char arr[21];
-	int rest;
-	cin.getline(arr,21);
-	cout<<arr<<" "<<rest;
+	  
+	int arr[10], num;
+	
+	char check[2];
+	
+	for(int i = 0; i<10; ++i) std::cin>>arr[i];
+	
+	std::cin.getline(check,2);
+	
+	if(check[0]!='\0'){
+		
+		std::cout<<"Enter 10 numbers";
+		
+		return 0;
+	}
+
+	for(int i = 0; i< 10; ++i) {
+		
+		if((num = arr[i]) < 0 || num > 9){
+
+			std::cout<<"Number must be between 0~9";
+			
+			return 0;
+
+		}
+
+	}
+
+	arrayModify(arr,10);
+	printArray(arr,10);
+	
 	return 0;
 }
